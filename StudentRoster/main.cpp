de9e8e8b07cc317479ba1a1cc2213d8b827e9e85
Max Sealey <maxsealey@users.noreply.github.com>
2022-07-14 16:21:05 -0700

    Initial Commit

---
+++ StudentRoster/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  StudentRoster
+//
+//  Created by Max Sealey on 7/14/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
