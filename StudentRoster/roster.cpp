6db2a75fc1625647ceae6508fc7c81882164afce
Max Sealey <maxsealey@users.noreply.github.com>
2022-10-09 22:00:15 -0700

    changed file names, created Roster class, added output to main function

---
+++ StudentRoster/roster.cpp
@@ -0,0 +1,8 @@
+//
+//  roster.cpp
+//  StudentRoster
+//
+//  Created by Max Sealey on 10/9/22.
+//
+
+#include "roster.h"
---
+++ StudentRoster/roster.h
@@ -0,0 +1,19 @@
+//
+//  roster.h
+//  StudentRoster
+//
+//  Created by Max Sealey on 10/9/22.
+//
+
+#ifndef roster_h
+#define roster_h
+
+#include <string>
+#include "degree.h"
+
+class Roster {
+    
+};
+
+
+#endif /* roster_h */
